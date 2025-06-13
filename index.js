a5f01e7cead357c56e1ae20740ed0b6bc7278032
Gloria Birundu <birundugloria@gmail.com>
2025-06-13 13:59:27 +0300

    first-class-functions-lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+console.log(returnFirstTwoDrivers(drivers)); // ['Antonia', 'Nuru']
+console.log(returnLastTwoDrivers(drivers));  // ['Amari', 'Mo']
+
+console.log(fareDoubler(10)); // 20
+console.log(fareTripler(10)); // 30
+
+console.log(selectDifferentDrivers(drivers, returnFirstTwoDrivers)); // ['Antonia', 'Nuru']
+console.log(selectDifferentDrivers(drivers, returnLastTwoDrivers));  // ['Amari', 'Mo']
